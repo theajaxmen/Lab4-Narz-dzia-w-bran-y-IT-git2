528c0d594352075a9ef15d7799805733d1dce760
s <s>
2024-07-21 14:44:22 +0200

    Add error

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+REPO_URL="https://github.com/theajaxmen/Lab4-Narz-dzia-w-bran-y-IT-git2"
+
 if [ "$1" == "--date" ] || [ "$1" == "-d" ]; then
     date
 elif [ "$1" == "--logs" ] || [ "$1" == "-l" ]; then
@@ -20,4 +22,14 @@ elif [ "$1" == "--init" ]; then
     git clone $REPO_URL
     echo "export PATH=\$PATH:$(pwd)//Lab4-Narz-dzia-w-bran-y-IT-git2" >> ~/.bashrc
     source ~/.bashrc
+elif [ "$1" == "--error" ] || [ "$1" == "-e" ]; then
+    count=100
+    if [ ! -z "$2" ]; then
+        count=$2
+    fi
+    mkdir -p error
+    for i in $(seq 1 $count); do
+        filename="error/error${i}.txt"
+        echo "Filename: $filename, Script: $0, Date: $(date)" > $filename
+    done
 fi
