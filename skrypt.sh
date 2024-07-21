cc63a019e77ac3d27bd1ab3590651a8bb9f63cf4
s <s>
2024-07-21 14:26:57 +0200

    Opcja log

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,13 @@
 
 if [ "$1" == "--date" ]; then
     date
+elif [ "$1" == "--logs" ]; then
+    count=100
+    if [ ! -z "$2" ]; then
+        count=$2
+    fi
+    for i in $(seq 1 $count); do
+        filename="log${i}.txt"
+        echo "Filename: $filename, Script: $0, Date: $(date)" > $filename
+    done
 fi
